fef8ccca11725d9dacfe4f407ed8e6f303405905
Abdo <abdoibrahim257@gmail.com>
2021-06-02 20:55:52 +0200

    Revert "klam kbeer"
    This reverts commit aabfbeb433e43a8ea3fc6c9c12d7fe6ab2507be5.

--- Phase2 - Project Framework/ApplicationManager.cpp
+++ Phase2 - Project Framework/ApplicationManager.cpp
@@ -33,88 +33,22 @@ void ApplicationManager::ExecuteAction(ActionType ActType)
 	//According to Action Type, create the corresponding action object
 	switch (ActType)
 	{
-	case DRAW_RECT:
-		pAct = new AddRectAction(this);
-		break;
+		case DRAW_RECT:
+			pAct = new AddRectAction(this);
+			break;
 
-	case DRAW_LINE:
-		//pAct = new AddLineAction(this);
-		break;
+		case DRAW_LINE:
+			///create AddLineAction here
 
-	case DRAW_TRI:
-		//pAct = new AddTriAction(this);
-		break;
+			break;
 
-	case DRAW_CIRC:
-		//pAct = new AddCircAction(this);
-		break;
-
-	case DRAW_ITEM:	//Draw an item    //MESH 3AREF LEHA LAZMA WLA EH
-		//pAct = new AddITEMAction(this);
-		break;
-
-	case CHNG_DRAW_CLR: //Change the drawing color
-		//pAct = new CHNG_DRAW-CLRAction(this);
-		break;
-
-	case CHNG_FILL_CLR:	//Change the filling color
-		//pAct = new CHNG_FILL-CLRAction(this);
-		break;
-
-	case CHNG_BK_CLR:	//Change background color
-		//pAct = new CHNG_BK-CLRAction(this);
-		break;
-
-	case SELECT:		//Select an item
-		//pAct = SELECTAction(this);
-		break;
-
-	case MOVE:			//Move a figure(s)
-		//pAct = new MOVEAction(this);
-		break;
-
-	case RESIZE:		//Resize a figure(s)
-		//pAct = new RESIZEAction(this);
-		break;
-
-	case DEL:			//Delete a figure(s)
-		//pAct = new DElAction(this);
-		break;
-
-	case COPY:           //Copy an item to Clipboard
-		//pAct = new COPYAction(this);
-		break;
-
-	case PASTE:         //Paste an item from Clipboard
-		//pAct = new PASTEAction(this);
-		break;
-
-	case CUT:            //Cut an item and have it in Clipboard
-		//pAct = new CUTAction(this);
-		break;
-
-	case SAVE:			//Save the whole graph to a file
-		//pAct = new SAVEAction(this);
-		break;
-
-	case LOAD:			//Load a graph from a file
-		//pAct = new LOADAction(this);
-		break;
-
-	case ZOOM_IN:        //Zooming the whole graph in
-		//pAct = new Zoom_INAction(this);
-		break;
-
-	case ZOOM_OUT:       //Zooming the whole graph out
-		//pAct = new Zoom_OUTAction(this);
-		break;
-
-	case EXIT:			//Exit the application
-		//pAct = new EXITAction(this);
-		break;
+		case EXIT:
+			///create ExitAction here
+			
+			break;
 		
-	case STATUS:	//a click on the status bar ==> no action
-		return;
+		case STATUS:	//a click on the status bar ==> no action
+			return;
 	}
 	
 	//Execute the created action
@@ -136,7 +70,7 @@ void ApplicationManager::AddFigure(CFigure* pFig)
 		FigList[FigCount++] = pFig;	
 }
 ////////////////////////////////////////////////////////////////////////////////////
-CFigure *ApplicationManager::GetFigure(int x, int y) const // ll select
+CFigure *ApplicationManager::GetFigure(int x, int y) const
 {
 	//If a figure is found return a pointer to it.
 	//if this point (x,y) does not belong to any figure return NULL
