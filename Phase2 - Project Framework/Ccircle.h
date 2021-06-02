219e1ec3a5c893f1b3eceea14a6709ca8762061b
Abdo <abdoibrahim257@gmail.com>
2021-06-02 23:48:55 +0200

    lol bardo

---
+++ Phase2 - Project Framework/Ccircle.h
@@ -0,0 +1,2 @@
+#pragma once
+#include "CFigure.h"
--- Phase2 - Project Framework/Figures/CRectangle.cpp
+++ Phase2 - Project Framework/Figures/CRectangle.cpp
@@ -1,12 +1,11 @@
 #include "CRectangle.h"
 
-CRectangle::CRectangle(Point P1, Point P2, GfxInfo FigureGfxInfo):CFigure(FigureGfxInfo)
+CRectangle::CRectangle(Point P1, Point P2, GfxInfo FigureGfxInfo):CFigure(FigureGfxInfo) //constructor
 {
 	Corner1 = P1;
 	Corner2 = P2;
 }
 	
-
 void CRectangle::Draw(Output* pOut) const
 {
 	//Call Output::DrawRect to draw a rectangle on the screen	
